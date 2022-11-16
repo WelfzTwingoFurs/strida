0e19154e0a26147c8ce414308f8b27baab041769
weg <60985201+WelfzTwingoFurs@users.noreply.github.com>
2022-11-15 23:30:17 -0300

    Add files via upload

--- Globalls.gd
+++ Globalls.gd
@@ -6,21 +6,15 @@ var player
 var audio
 var camera = Camera2D
 
-var maxi = false
 var step = 0
 var zoom = 3
 
 var was_pos
-var was_win
 
 func _process(_delta):
 	if step == 0: # Viewport, Keep -- Window size = monitor, image stretches, keep aspect-ratio
 		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, Vector2(OS.window_size.x, OS.window_size.y))
-		if maxi:
-			OS.set_window_maximized(false)
-			OS.set_window_maximized(true)
-		else:
-			was_pos = OS.get_window_position()
+		was_pos = OS.get_window_position()
 		step = 1
 	
 	elif step == 1: # Disabled, Ignore -- Window size /= zoom (zoom part 1), image doesn't stretch, keep vertical aspect-ratio
@@ -32,15 +26,9 @@ func _process(_delta):
 	elif step == 2: # Viewport, Keep --  Window size *= zoom (zoom OK), image stretches, keep aspect-ratio
 		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, Vector2(OS.window_size.x, OS.window_size.y))
 		OS.window_size *= zoom 
-		if maxi:
-			OS.set_window_maximized(false)
-			OS.set_window_maximized(true)
-		else:
-			OS.window_position = was_pos
+		OS.window_position = was_pos
 		
-		was_win = OS.get_real_window_size()
 		step = -1
-	
 	
 	if Input.is_action_just_pressed("cam_minus") && zoom > 1:
 		zoom -= 1
@@ -52,14 +40,10 @@ func _process(_delta):
 			zoom = 1
 		step = 0
 	
-	elif Input.is_action_just_pressed("bug_resmaxi"):
-		maxi = !maxi
-		step = 0
+
 	
 	elif Input.is_action_pressed("bug_resreset"):
 		step = 0
-	
-	
 	
 	
 	if Input.is_action_just_pressed("bug_reset"):#F5
